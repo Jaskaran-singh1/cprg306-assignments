24aeb0acb03fbb5174ab6a96f03999a882537387
Jaskaran Singh <130739668+Jaskaran-singh1@users.noreply.github.com>
2024-01-28 22:26:34 -0700

    Week-2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Jaskaran Singh</h1>
+            <Link href="https://github.com/Jaskaran-singh1/cprg306-assignments">https://github.com</Link>
+        </main>
+    );
+}
