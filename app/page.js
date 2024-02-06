b64836b84c1c5b393e63057effba132ca8070210
Jaskaran Singh <130739668+Jaskaran-singh1@users.noreply.github.com>
2024-02-06 16:50:14 -0700

    Week 3 Assignment

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
 import Link from "next/link";
 
 export default function Page() {
-    return (
-      <main>
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
-        <Link href="week-2">Week 2 Assignment</Link>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1 className="text-4xl font-bold mt-44 ml-44 mb-11">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <Link
+        href="week-2"
+        className="ml-52 text-xl hover:text-rose-700 hover:underline"
+      >
+        Week 2 Assignment
+      </Link>
+      <br />
+      <Link
+        href="week-3"
+        className="ml-52 text-xl hover:text-rose-700 hover:underline"
+      >
+        Week 3 Assignment
+      </Link>
+    </main>
+  );
+}
